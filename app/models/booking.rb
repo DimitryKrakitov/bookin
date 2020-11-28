cb10a10318dac87ba467078fd3361ad94fa81f0f
DimitryKrakitov <pedroneves95@gmail.com>
2020-11-28 16:56:17 +0000

    add validations to booking and space models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,2 +1,20 @@
 class Booking < ApplicationRecord
+  belongs_to :space
+  belongs_to :user
+
+  validates :user_id, presence: true
+  validates :space_id, presence: true
+
+  validates :start, presence: true
+  validates :finish, presence: true
+
+  validates_numericality_of :finish, greater_than: :start, message: ' must be after start'
+
+
+  # private
+
+  # def valid_time_slot
+  #   errors.add(:stop, "stop date cannot be older than start date") if stop < end
+  # end
+
 end
--- app/models/space.rb
+++ app/models/space.rb
@@ -1,2 +1,8 @@
 class Space < ApplicationRecord
+
+  validates :name, presence: true, uniqueness: true
+  validates :capacity, presence: true
+
+  validates_numericality_of :capacity, greater_than: 0
+
 end
